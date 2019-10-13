62ffdeb9b7d74581ed7ec8387de2b9e24e663328
Alexander Fürst <alexander@fuerst.one>
2019-10-13 21:14:19 +0200

    Update Array.vue

--- src/views/Array.vue
+++ src/views/Array.vue
@@ -95,27 +95,26 @@
                 animation: [
                     [0, 0, 0, 'Und los! Nimm schnell die Bestellung auf!'],
                     [0, 1, 0, 'Die Bestellung beinhaltet:'],
-                    [3, 0, 1, '1 x Cheeseburger']
-                    [4, 0, 2, '1 x Double-Cheeseburger']
-                    [5, 0, 3, '...und noch \'n Hamburger']
-                    [5, 0, 3, 'Lecker!']
-                    [5, 1, 0, 'Um die Bestellung jetzt an die Küche zu geben...']
-                    [2, 0, 0, '...verwendest du einfach den Array']
-                    [2, 0, 0, 'Dabei werden alle Items eingepflegt...']
-                    [2, 0, 0, '...und durch Indizes adressiert']
-                    [3, 2, 1, 'Cheeseburger beginnt mit Index 0']
-                    [4, 2, 2, 'Hier dann Index 1']
-                    [5, 2, 3, '...und so weiter']
-                    [5, 2, 3, 'Perferkt!']
-                    [5, 2, 3, 'Jetzt ist alles drin.']
-                    [8, 0, 0, 'Oh!']
-                    [8, 3, 0, 'Da hat es jemand wohl vercheckt.']
-                    [9, 0, 0, 'Für uns aber kein Problem!']
-                    [9, 0, 0, 'Mittels Index einfach das Feld bestimmen und - ']
-                    [11, 0, 0, 'Einfach auslesen!']
-                    [13, 2, 3, 'Kein Ding für \'n King!']
-                    [16, 0, 0, 'Darf es noch etwas sein?']
-
+                    [3, 0, 1, '1 x Cheeseburger'],
+                    [4, 0, 2, '1 x Double-Cheeseburger'],
+                    [5, 0, 3, '...und noch \'n Hamburger'],
+                    [5, 0, 3, 'Lecker!'],
+                    [5, 1, 0, 'Um die Bestellung jetzt an die Küche zu geben...'],
+                    [2, 0, 0, '...verwendest du einfach den Array'],
+                    [2, 0, 0, 'Dabei werden alle Items eingepflegt...'],
+                    [2, 0, 0, '...und durch Indizes adressiert'],
+                    [3, 2, 1, 'Cheeseburger beginnt mit Index 0'],
+                    [4, 2, 2, 'Hier dann Index 1'],
+                    [5, 2, 3, '...und so weiter'],
+                    [5, 2, 3, 'Perferkt!'],
+                    [5, 2, 3, 'Jetzt ist alles drin.'],
+                    [8, 0, 0, 'Oh!'],
+                    [8, 3, 0, 'Da hat es jemand wohl vercheckt.'],
+                    [9, 0, 0, 'Für uns aber kein Problem!'],
+                    [9, 0, 0, 'Mittels Index einfach das Feld bestimmen und - '],
+                    [11, 0, 0, 'Einfach auslesen!'],
+                    [13, 2, 3, 'Kein Ding für \'n King!'],
+                    [16, 0, 0, 'Darf es noch etwas sein?'],
                 ],
                 burgerAnimation: [
                     [],
@@ -123,7 +122,6 @@
                     [ 'bottomBun', 'patty', 'salad', 'tomatoes', 'cheese1', 'cheese2', 'sauce', 'topBun' ],
                     [ 'bottomBun', 'patty', 'salad', 'tomatoes', 'sauce', 'topBun' ],
                 ],
-
                 exercise: [
                     [ "$order", "=", "[", ],
                     [ "\"Double-Cheeseburger\", \"Coke\", \"Hamburger\", \"Milkshake\"", ],
